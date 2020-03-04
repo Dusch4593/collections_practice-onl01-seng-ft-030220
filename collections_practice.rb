38c5ab65c1cfa8773d7957e5c2c2e4f5dcf44bf1
Brandon Dusch <brandondusch@gmail.com>
2020-03-04 19:28:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,13 +16,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  def swap_elements_from_to(array, index, destination_index)
-    temp = array[index]
-    array[index] = array[destination_index]
-    array[destination_index] = temp
-    array
-  end
-  return swap_elements_from_to(array, 1, 2)
+  array[]
 end
 
 
